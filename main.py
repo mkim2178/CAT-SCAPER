3241d84c87dea56d0de98e4a71ad78df0e9af61e
koreanwizard <harry992178@gmail.com>
2025-06-22 13:49:37 +0900

    change the logic of the game. add a restart button if game is over (game is over when there is a collision with player and the enemy)

--- main.py
+++ main.py
@@ -1,37 +1,67 @@
 import pygame
 from player import Player
+from enemy import Enemy
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
-BG_COLOR = (166, 224, 206)
+BG_COLOR = (48, 48, 48)
 
 def main():
     pygame.init()
+    font = pygame.font.Font(None, 36)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     running = True
+    game_over = False
 
     player = Player(SCREEN_WIDTH, SCREEN_HEIGHT)
+    enemy = Enemy(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+
     all_sprites = pygame.sprite.Group()
-    all_sprites.add(player)
+    all_sprites.add(player, enemy)
+    for sprite in all_sprites:
+        print(sprite)
 
     while running:
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+
+
         
-        # update every sprite
-        all_sprites.update()
 
-        # draw the screen
-        screen.fill(BG_COLOR)
+        if game_over:
+            screen.fill((0, 0, 0))
+            text_surface = font.render("Game Over! Press R to restart.", True, (255, 0, 0))
+            text_rect = text_surface.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+            screen.blit(text_surface, text_rect)
 
-        # draw every sprite
-        all_sprites.draw(screen)
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_r]:
+                game_over = False
+                player.reset()
+                enemy.reset()
+        
+        else:
+
+            all_sprites.update()
+
+            # if there is a collide, game is over
+            if player.rect.colliderect(enemy):
+                game_over = True
+
+            screen.fill(BG_COLOR)
+            all_sprites.draw(screen)
+
+
 
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(60)  # Limit to 60 FPS
 
     pygame.quit()
 
