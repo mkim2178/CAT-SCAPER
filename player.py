9ef747678f5bcef6430f1dec9b103fc242991a8d
koreanwizard <harry992178@gmail.com>
2025-06-22 15:08:27 +0900

    player's update will use rect.x and rect.y to make it more general

--- player.py
+++ player.py
@@ -1,8 +1,4 @@
 import pygame
-import random
-
-PLAYER_SIZE = 30
-PLAYER_SPEED = 10
 
 class Player(pygame.sprite.Sprite):
 
@@ -10,70 +6,47 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
 
         # load the image to set the player's appearance -> this will be used when player's image has been selected
-        # self.image = pygame.image.load("bat_optimized.png").convert_alpha()
-
-        self.image = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE))
-        self.image.fill((30, 30, 30))
+        self.image = pygame.image.load("images/mouse4x.png").convert_alpha()
 
         # get the rectangle of the image
-        self.rect = self.image.get_rect()
-        
-        # set the player's starting point
-        self.rect.topleft = (random.randint(0, screen_width - PLAYER_SIZE), random.randint(0, screen_height - PLAYER_SIZE))
+        self.rect = self.image.get_rect(center=(320, 350))
 
         # set the boundaries that player can move
-        self.x_max_boundary = screen_width - PLAYER_SIZE
-        self.y_max_boundary = screen_height - PLAYER_SIZE
+        self.screen_width = screen_width
+        self.screen_height = screen_height
 
-        
+        self.speed = 7
 
     def update(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
 
-            if self.rect.left + PLAYER_SIZE <= self.x_max_boundary:
-                self.rect.left += PLAYER_SPEED
+            if self.rect.x + self.speed + self.rect.width <= self.screen_width:
+                self.rect.x += self.speed
             else:
-                self.rect.left = self.x_max_boundary
+                self.rect.x = self.screen_width - self.rect.width
 
         if keys[pygame.K_LEFT]:
 
-            if self.rect.left - PLAYER_SIZE >= 0:
-                self.rect.left -= PLAYER_SPEED
+            if self.rect.x - self.speed >= 0:
+                self.rect.x -= self.speed
             else:
-                self.rect.left = 0
-
+                self.rect.x = 0
             
         if keys[pygame.K_UP]:
 
-            if self.rect.top - PLAYER_SIZE >= 0:
-                self.rect.top -= PLAYER_SPEED
+            if self.rect.y - self.speed >= 0:
+                self.rect.y -= self.speed
             else:
-                self.rect.top = 0
+                self.rect.y = 0
 
         if keys[pygame.K_DOWN]:
 
-            if self.rect.top + PLAYER_SIZE <= self.y_max_boundary:
-                self.rect.top += PLAYER_SPEED
+            if self.rect.y + self.speed + self.rect.height <= self.screen_height:
+                self.rect.y += self.speed
             else:
-                self.rect.top = self.y_max_boundary
-        
+                self.rect.y = self.screen_height - self.rect.height
 
-
-
-
-
-    def test_random_starting_points(self):
-
-        for i in range(100):
-
-            x = random.randint(0, 1260)
-            y = random.randint(0, 700)
-
-            if x < 0 or x > 1260:
-                print(f"WRONG X_POS: {x}")
-            if y < 0 or y > 700:
-                print(f"WRONG Y_POS: {y}")
-            if 0 <= x <= 1260 and 0 <= y <= 700:
-                print(f"CORRECT X AND Y: {x, y}")
+    def reset(self):
+        self.rect.center = (320, 350)
