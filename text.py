7212331c0fdbd66fd01b040851cb6005e5676dd7
thisisminu <minwookim1105@gmail.com>
2025-06-23 16:16:11 +0900

    add a Text class to add a text on the game

---
+++ text.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Text:
+
+    def __init__(self, text, font_size, font_color, font_center_pos, font_type=None):
+        self.text = text
+        self.font = pygame.font.Font(font_type, font_size)
+        self.font_color = font_color
+        self.surface = self.font.render(text, True, self.font_color)
+        self.rect = self.surface.get_rect(center=font_center_pos)
+    
+    def draw(self, target_surface):
+        target_surface.blit(self.surface, self.rect)
+
+    def update(self, new_text):
+        self.text = new_text
+        self.surface = self.font.render(new_text, True, self.font_color)
+        self.rect = self.surface.get_rect(center=self.rect.center)
+
+    def set_center(self, center_pos):
+        self.rect = self.surface.get_rect(center=center_pos)
