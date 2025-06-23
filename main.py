ecc702de6b988e3cdcef502774e01f6173a71176
thisisminu <minwookim1105@gmail.com>
2025-06-23 16:15:35 +0900

    fix the logic of creating a text and button for restarting the game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from player import Player
 from enemy import Enemy
+from text import Text
+from button import Button
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
@@ -8,7 +10,6 @@ BG_COLOR = (48, 48, 48)
 
 def main():
     pygame.init()
-    font = pygame.font.Font(None, 36)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     running = True
@@ -16,6 +17,15 @@ def main():
 
     player = Player(SCREEN_WIDTH, SCREEN_HEIGHT)
     enemy = Enemy(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+
+
+
+
+
+    restart_text = Text("Restart", 36, (0, 0, 0), (0, 0))
+    restart_button = Button((SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2), 180, 50, (255, 255, 255), restart_text, (0, 255, 0))
+
 
 
     all_sprites = pygame.sprite.Group()
@@ -32,25 +42,24 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     running = False
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    restart_button.is_clicked(event.pos)
+                    print("CLICK")
+                    game_over = False
+                    player.reset()
+                    enemy.reset()
+                
+
+            
+
 
         
 
         if game_over:
+
             screen.fill((0, 0, 0))
-
-            # restart with a restart button
-            button_rect = pygame.Rect(0, 0, int(SCREEN_WIDTH * 0.2), int(SCREEN_HEIGHT * 0.1))
-            button_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-            button_color = (70, 130, 180)
-            button_text = "Restart"
-            font = pygame.font.Font(None, 36)
-            pygame.draw.rect(screen, button_color, button_rect)
-
-            text_surface = font.render(button_text, True, (255, 255, 255))
-            text_rect = text_surface.get_rect(center=button_rect.center)
-            screen.blit(text_surface, text_rect)
-
-
+            restart_button.draw(screen)
 
 
             # restart with r_key
@@ -61,9 +70,7 @@ def main():
 
             # keys = pygame.key.get_pressed()
             # if keys[pygame.K_r]:
-            #     game_over = False
-            #     player.reset()
-            #     enemy.reset()
+
         
         else:
 
